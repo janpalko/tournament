dacaacf50a4cb755dd2d47f477bb3c1f2ecefa35
Jan Palko <jan.palko@gmail.com>
2021-06-22 20:14:21 +0200

    Clean up

--- src/main/java/sk/palko/tournament/repository/MatchRepository.java
+++ src/main/java/sk/palko/tournament/repository/MatchRepository.java
@@ -1,8 +1,10 @@
 package sk.palko.tournament.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import sk.palko.tournament.domain.Match;
 
+@Repository
 public interface MatchRepository extends CrudRepository<Match, Integer> {
 
   long countByResultIsNull();
--- src/main/java/sk/palko/tournament/repository/PlayerRepository.java
+++ src/main/java/sk/palko/tournament/repository/PlayerRepository.java
@@ -2,8 +2,10 @@ package sk.palko.tournament.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import sk.palko.tournament.domain.Player;
 
+@Repository
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
 
   @Query("""
